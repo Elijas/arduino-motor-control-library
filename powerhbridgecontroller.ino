47e476c75086e55cf66126807b941ae4bee08aff
Elijas <master.elijas@gmail.com>
2014-08-13 21:50:20 +0300

    gradual speed increase, analog input read

--- powerhbridgecontroller.ino
+++ powerhbridgecontroller.ino
@@ -1,31 +1,78 @@
+#include <Timer.h>
+
 #define PIN_MOTOR_FWD 2
 #define PIN_MOTOR_BWD 3
-#define PIN_ANALOGREAD_1 A14
-#define PIN_ANALOGREAD_2 A15
+#define PIN_CURRENTCHECK_1 A3
+#define PIN_CURRENTCHECK_2 A4
 
+Timer t;
 int powerOfTen(int);
 int readPosNumberFromSerial();
+void readSerialInput();
+
+
+int currPower = 0;
+int targetPower = 0;
+int a1sum,a2sum; //for better visibility
+
+
+void readAnalogs() {
+    int a1 = analogRead(PIN_CURRENTCHECK_1), a2 = analogRead(PIN_CURRENTCHECK_2);
+    a1sum+=a1;
+    a2sum+=a2;
+    /*
+    Serial.print(currPower); 
+    Serial.print(" "); 
+    Serial.print(a1); 
+    Serial.print(" "); 
+    Serial.println(a2);
+    */
+}
+
+void printSums() {
+    Serial.print(currPower); 
+    Serial.print(" "); 
+    Serial.print(a1sum); 
+    Serial.print(" "); 
+    Serial.println(a2sum);
+    
+    a1sum=a2sum=0;
+}
+
+void updateMotors() {
+    if (currPower < targetPower) analogWrite(PIN_MOTOR_FWD, ++currPower);
+    else if (currPower > targetPower) analogWrite(PIN_MOTOR_FWD, --currPower);
+}
+
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(57600);
     pinMode(PIN_MOTOR_FWD, OUTPUT);
     pinMode(PIN_MOTOR_BWD, OUTPUT);
+    
+    t.every(1000, readSerialInput);
+    t.every(100, updateMotors);
+    t.every(1, readAnalogs);
+    t.every(100, printSums);
 }
+void loop() {t.update();}
 
-void loop() {
-    int targetPower = readPosNumberFromSerial();   
-    if (targetPower != -1) {
-        analogWrite(PIN_MOTOR_FWD, targetPower);
-        Serial.println(targetPower);
+
+
+
+/// SERIAL COMMS
+void readSerialInput() {
+    int serialInput = readPosNumberFromSerial();   
+    if (serialInput != -1) {
+        if (serialInput < 0 && serialInput > 240) targetPower = 0; //safeguard against unexpected values
+        else targetPower = serialInput;
+        //Serial.println(targetPower);
     }
-    delay(1000);
 }
-
 int powerOfTen(int a) {
     if (a>0) return 10*powerOfTen(a-1);
     else return 1;
 }
-
 int readPosNumberFromSerial() {
     if (Serial.available()) {
         int number = 0;
